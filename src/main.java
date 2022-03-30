48bd66ea211a14b652edd3a67aa015e2f7649835
Tehese <Scanfiel@uccs.edu>
2022-03-30 11:49:16 -0600

    Added Test Files for Syntactic, Updated Main/Syntactic

--- src/main.java
+++ src/main.java
@@ -1,39 +1,26 @@
+/*
+Scott Canfield
+CS 4100
+Compilers Spring 2022
+ */
 import ADT.*;
 
 public class main {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-            System.out.println("Scott Canfield\n");
-            System.out.println("Last 4 of Student ID: 2339");
-            System.out.print("CS4100 Spring 2022 Compiler = IntellJ");
+        System.out.println("Scott Canfield");
+        System.out.println("Last 4 of Student Id: 2339");
+        System.out.println("CS 4100 Compilers");
+        System.out.println("Spring 2022");
+        System.out.println("IDE: IntelliJ");
 
+        String filePath = "/Users/tehese/IdeaProjects/CS-4100-Compilers/src/Test Files/GoodSyntaxASP22.txt";
+        boolean traceon = true;
+        Syntactic parser = new Syntactic(filePath, traceon);
+        parser.parse();
+        System.out.println("Done.");
+        }
 
-
-            // /Users/tehese/IdeaProjects/CS-4100-Compilers/src/Test Files/LexicalTestSP22.txt MAC FILE ADDRESS
-            // C:\Users\someb\JavaProjects\CS4100_Compilers\src\Test Files\LexicalTestSP22.txt PC FILE ADDRESS
-
-            String fileAndPath = "/Users/tehese/IdeaProjects/CS-4100-Compilers/src/Test Files/LexicalTestSP22.txt";
-            System.out.println("Lexical for " + fileAndPath);
-            boolean traceOn = true;
-            // Create a symbol table to store appropriate3 symbols found
-            SymbolTable symbolList;
-            symbolList = new SymbolTable(150);
-            Lexical myLexer = new Lexical(fileAndPath, symbolList, traceOn);
-            Lexical.token currToken;
-            currToken = myLexer.GetNextToken();
-            //Mnemonic | Code | Lexeme
-            while (currToken != null) {
-                    System.out.println("\t" + currToken.mnemonic + " | \t" +
-                            String.format("%04d", currToken.code)
-                            + " | \t" + currToken.lexeme);
-                    currToken = myLexer.GetNextToken();
-            }
-
-            // /Users/tehese/IdeaProjects/CS-4100-Compilers/src/Output Files/symboltable2.txt MAC File Address
-            // C:\Users\someb\JavaProjects\CS4100_Compilers\src\Output Files\symboltable2.txt PC File Address
-            
-            symbolList.PrintSymbolTable("/Users/tehese/IdeaProjects/CS-4100-Compilers/src/Output Files/symboltable2.txt");
-            System.out.println("Done.");
-    }
 }
+
